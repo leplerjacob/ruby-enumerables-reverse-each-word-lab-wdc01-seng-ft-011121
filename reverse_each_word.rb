1384bb1f09d481abd3ca9740f1b4276ad84a5f99
Jacob Lepler <lepler.jacob@gmail.com>
2021-01-07 22:32:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,31 @@
+# def reverse_each_word(string)
+  
+#   # print string
+#   # p string.split(" ").each{|word| word.reverse!}.join(" ")
+  
+#   p string.split(" ").collect{|word| word.reverse!}.join(" ")
+  
+  
+  
+# end
+require 'pry'
 def reverse_each_word(string)
-  
-  # print string
-  # p string.split(" ").each{|word| word.reverse!}.join(" ")
-  
-  p string.split(" ").collect{|word| word.reverse!}.join(" ")
-  
+  string_to_array = string.split(" ")
+  new_reverse_array = []
+  string_to_array.each do |el|
+    new_reverse_array << el.reverse
+  end
+new_reverse_array.join(" ")
 end
+
+
+
+
+
+
+
+
+
+
+
+
